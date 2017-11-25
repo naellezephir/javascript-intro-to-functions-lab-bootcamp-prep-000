5f9da55bc2c45da0aa3ba588469846597be2aba7
Naelle Zephir <nzephir@bu.edu>
2017-11-25 04:45:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function shout(string){
   return string.toUpperCase()
 }
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
